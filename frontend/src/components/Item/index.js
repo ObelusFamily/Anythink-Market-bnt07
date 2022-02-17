8a141bdb306e334e88cc09a3fd818d030422ea6a
Lior Brauer <liorbrauer@gmail.com>
2022-02-17 15:46:21 +0200

    add placeholder image

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -1,5 +1,6 @@
 import ItemMeta from "./ItemMeta";
 import CommentContainer from "./CommentContainer";
+import placeholder from '../../imgs/placeholder.png'
 import React from "react";
 import agent from "../../agent";
 import { connect } from "react-redux";
@@ -50,7 +51,7 @@ class Item extends React.Component {
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image}
+                src={this.props.item.image || placeholder}
                 alt={this.props.item.title}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
